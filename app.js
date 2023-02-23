87a65322aaa5fb05c9782af8e656710cfb6b3e77
ifazlayrabbi <ifazlayrabbi@gmail.com>
2023-02-24 03:18:33 +0600

    commonjs vs module, express route parameters, ejs

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
 
+app.set('view engine', 'ejs')
+
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
 import {create_data, read_data, update_data, delete_data, read_all_data} from './database.js'
 
 
@@ -17,31 +23,38 @@ app.get('/', async (req, res) => {
   res.send(readAllData)
 })
 
-app.get('/:title_name', async (req, res) => {
+app.get('/get/:title_name', async (req, res) => {
   const titleName = req.params.title_name
   const readData = await read_data(titleName)
   res.send(readData)
 })
 
-app.get('/create', async (req, res) => {
-  // const titleName = req.params.title_name
-  // const readData = await read_data(titleName)
-  // res.send(readData)
-
-  res.sendFile('/create.html')
+app.get('/create', (req, res) => {
+  res.sendFile(__dirname + '/create.html')
+  // res.sendFile('D:/Work/ACodeX/A Web/projects/p04 - Web Course/back-end/08 SQL Database/create.html')
+  res.render('create')
 })
+
+
+
+app.post('/create', (req, res) => {
+  const title = req.body.title
+  const contents = req.body.contents
+
+})
+
+
+
+
+
+
+
 
 
 app.use((err, req, res, next) => {
   console.error(err.stack)
   res.status(500).send('Something broke!')
 })
-
-
-
-
-
-
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log('Server is running on port '+port))
---
+++ promise.js
@@ -0,0 +1,15 @@
+let promise = new Promise((resolve, reject) => {
+  let a = 1 + 1
+  if (a == 2) {
+    resolve('Succeed')
+  } else {
+    reject('Failed')
+  }
+})
+
+promise.then((message) => {
+  console.log('Promise is ' + message)
+}).catch((message) => {
+  console.log('Promise is ' + message)
+})
+
